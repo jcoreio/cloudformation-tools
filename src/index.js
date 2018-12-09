9e19aff35e808355c5c997f0a5c62592ae0832ed
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2018-12-09 13:30:01 -0600

    feat: export all scripts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // @flow
 
 export {default as deployCloudFormationStack} from './deployCloudFormationStack'
+export {default as describeCloudFormationFailure} from './describeCloudFormationFailure'
+export {default as getCurrentStackEvents} from './getCurrentStackEvents'
+export {default as getStackOutputs} from './getStackOutputs'
+export {default as getStackResources} from './getStackResources'
+export {default as printStackResources} from './printStackResources'
