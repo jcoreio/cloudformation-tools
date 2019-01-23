a4479dcf0897226cdeea59296283cfd681418ca4
Andy Edwards <jedwards@fastmail.com>
2019-01-22 19:14:38 -0600

    ci: try using @babel/plugin-proposal-async-generator-functions

--- .babelrc.js
+++ .babelrc.js
@@ -17,6 +17,7 @@ module.exports = function(api) {
   ]
 
   if (api.env(['test', 'coverage', 'es5'])) {
+    plugins.push('@babel/plugin-proposal-async-generator-functions')
     plugins.push('@babel/plugin-transform-runtime')
   }
   if (api.env('coverage')) {
