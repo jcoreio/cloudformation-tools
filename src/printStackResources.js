01d9421daef8a35d6da14992c15032c5ae0b1367
Andy Edwards <jedwards@fastmail.com>
2022-04-01 16:07:58 -0500

    fix(printStackResources): match blue IN_PROGRESS color used in AWS Console

--- src/printStackResources.js
+++ src/printStackResources.js
@@ -12,7 +12,7 @@ function formatStatus(status: string): string {
   if ('DELETE_COMPLETE' === status) return chalk.gray(status)
   if (/_COMPLETE$/.test(status)) return chalk.green(status)
   if (/_FAILED$/.test(status)) return chalk.red(status)
-  if (/_IN_PROGRESS$/.test(status)) return chalk.hex('#f90')(status)
+  if (/_IN_PROGRESS$/.test(status)) return chalk.hex('#0073bb')(status)
   return status
 }
 
